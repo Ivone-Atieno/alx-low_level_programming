92ffce5504aa772920c57f2333f6fd34f1a2df82
Ivone-Atieno <ivoneowuor@gmail.com>
2023-04-20 01:38:27 -0700

    task 4 and 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+*main() - It is the main entry point
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	/*
+	 *return - returns 0 if success
+	 */
+	return (0);
+}
