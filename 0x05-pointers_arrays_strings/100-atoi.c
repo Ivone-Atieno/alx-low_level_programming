3186e9aa959f574e64eae0e37f8a40e669195319
Ivone-Atieno <ivoneowuor@gmail.com>
2023-05-02 02:51:03 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@ int _atoi(char *s)
 {
 	int i = 0, n = 0, sign = 1;
 
-while ((s[i] < '0' && s[i] > '9') && s[i] != '0')
+while ((s[i] < '0' || s[i] > '9') && s[i] != '0')
 {
 	if (s[i] == '-')
 		sign *= -1;
@@ -26,7 +26,7 @@ while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 		n = n * 10 - (s[i] - '0');
 		i++;
 	}
-}
 	sign *= -1;
+}
 	return (n * sign);
 }
