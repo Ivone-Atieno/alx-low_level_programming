47153adce7418bfb93060b71901ece51675d28ec
Ivone-Atieno <ivoneowuor@gmail.com>
2023-05-12 08:08:37 -0700

    update on task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers
+ * main - main entry point
  * @argc: number count of arguments
  * @argv: array of arguments
  * Return: 0 for success else 1
@@ -10,17 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int res;
+	int num1, num2, res;
 
-if (argc != 3)
+	if (argc != 3)
 {
 	printf("Error\n");
 	return (1);
 }
-
-res = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	res = num1 * num2;
+	
 printf("%d\n", res);
 return (0);
 }
