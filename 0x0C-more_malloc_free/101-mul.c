df2301fc0771432ff2710f11ee3e1ab76e49b44e
Ivone-Atieno <ivoneowuor@gmail.com>
2023-05-18 10:49:42 -0700

    advanced task 2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,58 +14,59 @@
 
 int is_digit(char *num)
 {
-  int a = 0;
+	int a = 0;
 
-  for (; num[a] != '0'; a++)
-    {
-      if (!isdigit(num[a]))
+	for (; num[a] != '0'; a++)
 	{
-	  return (0);
+		if (!isdigit(num[a]))
+		{
+			return (0);
+		}
 	}
-    }
-  return (1);
+	return (1);
 }
 
 void print_num (signed long int numm)
 {
-  if (numm < 0)
-    {
-      _putchar('-');
-      numm = -numm;
-    }
-  if (numm / 10)
-    {
-      print_num (numm / 10);
-    }
-  _putchar((numm % 10) + '0');
+	if (numm < 0)
+	{
+		_putchar('-');
+		numm = -numm;
+	}
+
+	if (numm / 10)
+	{
+		print_num (numm / 10);
+	}
+	_putchar((numm % 10) + '0');
 }
 
 int main(int argc, char **argv)
 {
-  int num1, num2;
-  signed long int res;
+	int num1, num2;
+	signed long int res;
 
-  if (argc != 3)
-    {
-        _putchar('Error');
-	_putchar(10);
-	return (98);
-    }
+	if (argc != 3)
+	{
+		_putchar('Error');
+		_putchar(10);
+		return (98);
+	}
 
 if (!is_digit(argv[1]) || !is_digit(argv[2]))
-  {
-    _putchar('Error');
-    _putchar(10);
+{
+	_putchar('Error');
+	_putchar(10);
 	return (98);
-  }
+}
 
- num1 = atoi(argv[1]);
- num2 = atoi(argv[2]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
 
- res = (signed long int) num1 * num2;
- 
- print_num (res);
- _putchar(10);
+res = (signed long int) num1 * num2;
 
- return (0);
+print_num (res);
+_putchar(10);
+
+return (0);
 }
